eea36a168b49934f2b34078a2383f600b9f8b576
Youssef-Ahmed29 <Ya457551j@gmail.com>
2026-04-21 22:11:39 +0200

    Initial Comit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import NavBar from './Components/NavBar';
+import { Banner } from './Components/Banner';
+import { Skills } from './Components/Skills';
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+        
+    </div>
+  );
+}
+
+export default App;
